eefc5ed6461a589f03da8f1b4292e03839ec89ce
Matt Tyas <matthewtyas@hotmail.com>
2017-09-28 17:58:33 +0100

    tweaks

--- searchform.php
+++ searchform.php
@@ -2,10 +2,10 @@
 	<label for="s">Search</label>
 	<input type="text" value="" name="s" id="s" placeholder="Search finest">
 	<input class="icon-search" type="submit" id="searchsubmit" value="Search">
-	<div class="gw">
+	<!-- <div class="gw">
 		<div class="g one-third palm-one-whole">
 			<h3>Looking for something to do?</h3>
 			<p>Try our <a href="<?php echo get_site_url(); ?>/find/">directory search</a> of businesses in&nbsp;Manchester.</p>
 		</div>
-	</div>
+	</div> -->
 </form>
