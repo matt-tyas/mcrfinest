51dc14d1c5fe1c14a225dd73837766d90f570a0d
Matt Tyas <matthewtyas@hotmail.com>
2017-10-17 20:20:31 +0100

    rollback

--- logo.php
+++ logo.php
@@ -1,5 +1,4 @@
 <div class="site-title">
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/manchesters-finest.svg" alt="Manchester's Finest" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/images/manchesters-finest.png'">
-    </a>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">Manchester's <span>Finest</span><span class="accessibility"> and the Northern Quarter</span>
+	</a>
 </div>
