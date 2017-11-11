9c927db7a512f10cddd5eb2ce4ab2b049c42b194
Matt Tyas <matthewtyas@hotmail.com>
2017-11-11 06:42:42 +0000

    social footer

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 		<footer role="contentinfo" id="footer">
 			<div class="gw">
 				<div class="page">
-					<div class="g two-fifths palm-one-whole">
+					<div class="g one-quarter palm-one-whole">
 						<h5 class="sub-title">Thank you for your support</h5>
 						<div class="footer-text">
 							<p>
@@ -13,17 +13,17 @@
 							</p>
 						</div>
 					</div>
-					<div class="g one-fifth palm-one-whole">
+					<div class="g one-quarter palm-one-whole">
 						<h5 class="sub-title">Get involved</h5>
 						<?php wp_nav_menu( array( 'theme_location' => 'secondary-menu', 'menu_class' => 'nav nav--stacked' )); ?>
 					</div>
-					<div class="g one-fifth palm-one-half">
+					<div class="g one-quarter palm-one-half">
 						<?php
 							if ( is_active_sidebar( 'site-sponsor-3' ) ) : ?>
 							<?php dynamic_sidebar( 'site-sponsor-3' ); ?>
 						<?php endif; ?>
 					</div>
-					<div class="g one-fifth palm-one-half">
+					<div class="g one-quarter palm-one-half">
 						<div class="social-inline">
 							<?php get_template_part('social'); ?>
 						</div>
