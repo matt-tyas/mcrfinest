7ad4d1342383e99ff9b6e6dbb73e2030848cb74b
Matt Tyas <matthewtyas@hotmail.com>
2018-02-08 19:30:40 +0000

    breadcrumb directory

--- functions.php
+++ functions.php
@@ -1798,18 +1798,18 @@ function get_video_thumbnail_uri( $video_uri ) {
     add_filter('the_content', 'add_classes_to_linked_images', 100, 1);
 
 
-	function disable_wp_emojicons() {
-
-	  // all actions related to emojis
-	  remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-
-	  // filter to remove TinyMCE emojis
-	  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
-	}
-	add_action( 'init', 'disable_wp_emojicons' );
+	// function disable_wp_emojicons() {
+    //
+	//   // all actions related to emojis
+	//   remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	//   remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	//   remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	//   remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	//   remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	//   remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	//   remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    //
+	//   // filter to remove TinyMCE emojis
+	//   add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+	// }
+	// add_action( 'init', 'disable_wp_emojicons' );
