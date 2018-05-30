d3fe00171140b01d2e4feaf264c3c91607bfc68e
Matt Tyas <matthewtyas@hotmail.com>
2018-05-30 13:23:27 +0100

    nanny banton logo etc

--- category-video.php
+++ category-video.php
@@ -64,6 +64,8 @@ get_header(); ?>
                                         echo 'mf-pop--howtomake">';
                                     }if(has_category( 'Developing Manchester' )) {
                                         echo 'mf-pop--developingmanchester">';
+                                    }if(has_category( 'Nanny Banton and Friends' )) {
+                                        echo 'mf-pop--banton">';
                                     }elseif(has_category( '' )) {
                                         echo '';
                                     }
@@ -102,6 +104,10 @@ get_header(); ?>
                                         echo '<img src="';
                                         echo get_bloginfo('template_url');
                                         echo '/images/video/developing-manchester.svg"/>';
+                                    }if(has_category( 'Nanny Banton and Friends' )) {
+                                        echo '<img src="';
+                                        echo get_bloginfo('template_url');
+                                        echo '/images/video/banton-logo.svg"/>';
                                     }elseif(has_category( '' )) {
                                         echo '';
                                     }?>
